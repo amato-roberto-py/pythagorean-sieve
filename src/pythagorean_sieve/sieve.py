13f429deeac352b16c6f6a4f1463c3fb214e133b
amato-roberto-py <amato.roberto.py@gmail.com>
2026-01-12 12:12:13 +0100

    Update sieve.py

--- src/pythagorean_sieve/sieve.py
+++ src/pythagorean_sieve/sieve.py
@@ -122,3 +122,8 @@ def nth_prime_r(n, guess=5000):
             return ps[n-1]
         X *= 2
 
+if __name__ == "__main__":
+    plist = primes_up_to_r_segmented(100, segment_size=100_000)
+    print(plist)
+    print("count =", len(plist))
+    print("734th prime =", nth_prime_r(734))
